212278f8dd44f94b938421344edcb7153a746b5f
Tyler Danstrom <tdanstrom@uchicago.edu>
2017-01-11 12:12:17 -0600

    adjusted accession_landing.html to not require collection title or spcl id

--- inventorygeneration/generate_html.py
+++ inventorygeneration/generate_html.py
@@ -10,7 +10,8 @@ from hierarchicalrecord.hierarchicalrecord import HierarchicalRecord
 
 LONGTERM_ROOT = "/data/repository/longTermStorage"
 N = 2 
-URL_BASE = "https://y2.lib.uchicago.edu/processor/"
+ACCESSION_URL_BASE = "https://y2.lib.uchicago.edu/accession/"
+OBJECT_URL_BASE = "https://y2.lib.uchicago.edu/private/"
 
 ENV = Environment(loader=FileSystemLoader("./templates"))
 
@@ -19,73 +20,73 @@ if __name__ == "__main__":
     arkid_split =[arkid[i:i+N] for i in range(0, len(arkid), N)]
     reader = FileSystemArchiveReader(LONGTERM_ROOT, arkid)
     archive = reader.read()
-    files = []
-    segments = []
-    landing_page = namedtuple("landing_page", "accession_id collection_title description segments accession_record")
-    for segment in archive.segment_list:
-        for ms in segment.materialsuite_list:
-            iName_content = ms.content.item_name
-            arkid = str(ms.content.path).split('arf')[0].split('longTermStorage')[1].split('/')
-            premisid = str(ms.content.path).split('pairtree_root')[1].split('arf')[0].split('/')
-            arkid = ''.join(arkid)
-            premisid = ''.join(premisid)
-            x = namedtuple("an_item",
-                           "name contenturl premisurl fitsurl techmdsurl presformsurl presformurl")\
-                           (ms.content.item_name,
-                            URL_BASE + arkid + "/" + premisid + "/content",
-                            URL_BASE + arkid + "/" + premisid + "/premis",
-                            URL_BASE + arkid + "/"  + premisid + "/techmds/0",
-                            URL_BASE + arkid + "/" + premisid + "/techmds",
-                            URL_BASE + arkid + "/" + premisid + "/presforms",
-                            URL_BASE + arkid + "/" + premisid + "/presform")
-            files.append(x)
-        new_files_list = sorted(files, key=lambda x: x.name)
-        segment = namedtuple("segment", "id files numfiles")(segment.identifier, new_files_list, len(new_files_list))
-        segments.append(segment)
+    print(archive)
+    # files = []
+    # segments = []
+    # landing_page = namedtuple("landing_page", "accession_id collection_title description segments accession_record")
+    # for segment in archive.segment_list:
+    #     for ms in segment.materialsuite_list:
+    #         iName_content = ms.content.item_name
+    #         arkid = str(ms.content.path).split('arf')[0].split('longTermStorage')[1].split('/')
+    #         premisid = str(ms.content.path).split('pairtree_root')[1].split('arf')[0].split('/')
+    #         arkid = ''.join(arkid)
+    #         premisid = ''.join(premisid)
+    #         x = namedtuple("an_item",
+    #                        "name contenturl premisurl accessions legalnotes adminnotes")\
+    #                        (ms.content.item_name,
+    #                         URL_BASE + arkid + "/" + premisid + "/content",
+    #                         URL_BASE + arkid + "/" + premisid + "/premis",
+    #                         [],
+    #                         [],
+    #                         [])
+    #         files.append(x)
+    #     new_files_list = sorted(files, key=lambda x: x.name)
+    #     segment = namedtuple("segment", "id files numfiles")(segment.identifier, new_files_list, len(new_files_list))
+    #     segments.append(segment)
 
-    l = [x for x in archive.accessionrecord_list if x.item_name == "accession_record.json"]
-    h = HierarchicalRecord()
-    h.fromJSON(str(l[0].path))
-    mkdir("./" + arkid)
+    # l = [x for x in archive.accessionrecord_list if x.item_name == "accession_record.json"]
+    # h = HierarchicalRecord()
+    # h.fromJSON(str(l[0].path))
+    # mkdir("./" + arkid)
 
-    collection_title = h.get_field("Collection Title")
-    accession_id = h.get_field("Accession Identifier")
-    summary = h.get_field("Summary")
-    landing_page.accession_id = accession_id
-    landing_page.collection_title = collection_title
-    landing_page.description = summary
-    landing_page.segments = []
+    # collection_title = h.get_field("Collection Title")
+    # accession_id = h.get_field("Accession Identifier")
+    # summary = h.get_field("Summary")
+    # landing_page.accession_id = accession_id
+    # landing_page.collection_title = collection_title
+    # landing_page.description = summary
+    # landing_page.segments = []
 
-    for n_segment in segments:
-        a_seg = namedtuple("seg", "label numfiles")(n_segment.id, len(n_segment.files))
-        landing_page.segments.append(a_seg)
+    # for n_segment in segments:
+    #     a_seg = namedtuple("seg", "label numfiles")(n_segment.id, len(n_segment.files))
+    #     landing_page.segments.append(a_seg)
 
-    landing_page.accession_record = h.toJSON()
-    landing_template = ENV.get_template("accession_landing.html")
-    landing_html = landing_template.render(collection_title=collection_title[0], spcl_id=accession_id[0], ark_id=arkid, description=summary[0], segment_list=segments, fullaccessionrecord=h.toJSON())
+    # landing_page.accession_record = h.toJSON()
+    # landing_template = ENV.get_template("accession_landing.html")
+    # landing_html = landing_template.render(collection_title=collection_title[0], spcl_id=accession_id[0], ark_id=arkid, description=summary[0], segment_list=segments, fullaccessionrecord=h.toJSON())
 
-    with open(join(arkid, "index.html"), "w") as write_file:
-        write_file.write(landing_html)
+    # with open(join(arkid, "index.html"), "w") as write_file:
+    #     write_file.write(landing_html)
 
-    for n_segment in segments:
-        new_list = [segment.files[i:i+1000] for i in range(0, len(segment.files), 1000)]
-        page_numbers = [x for x in range(0, len(new_list))]
-        p = {}
-        p["pages"] = {}
-        for n_group in range(len(new_list)):
-            print(n_group)
-            a_dict = {"active":False, "startPoint":False}
-            p["pages"][n_group] = a_dict
+    # for n_segment in segments:
+    #     new_list = [segment.files[i:i+1000] for i in range(0, len(segment.files), 1000)]
+    #     page_numbers = [x for x in range(0, len(new_list))]
+    #     p = {}
+    #     p["pages"] = {}
+    #     for n_group in range(len(new_list)):
+    #         print(n_group)
+    #         a_dict = {"active":False, "startPoint":False}
+    #         p["pages"][n_group] = a_dict
 
-    for n_segment in segments:
-        last_page = len(new_list) - 1
-        for n_group in range(len(new_list)):
-            p["pages"][n_group]['active'] = True
-            p["pages"][n_group]['startPoint'] = True
-            json_string = dumps(p)
-            segment_template = ENV.get_template("section_list.html")
-            segment_html = segment_template.render(segment=n_segment, arkid=arkid, label=n_segment.id, files=new_list[n_group], pagerecord=json_string)
-            with open(join(arkid, n_segment.id + "-" + str(n_group) + ".html"), "w") as a_file_to_write:
-                a_file_to_write.write(segment_html)
-            p["pages"][n_group]['active'] = False
-            p["pages"][n_group]['startPoint'] = False
+    # for n_segment in segments:
+    #     last_page = len(new_list) - 1
+    #     for n_group in range(len(new_list)):
+    #         p["pages"][n_group]['active'] = True
+    #         p["pages"][n_group]['startPoint'] = True
+    #         json_string = dumps(p)
+    #         segment_template = ENV.get_template("section_list.html")
+    #         segment_html = segment_template.render(segment=n_segment, arkid=arkid, label=n_segment.id, files=new_list[n_group], pagerecord=json_string)
+    #         with open(join(arkid, n_segment.id + "-" + str(n_group) + ".html"), "w") as a_file_to_write:
+    #             a_file_to_write.write(segment_html)
+    #         p["pages"][n_group]['active'] = False
+    #         p["pages"][n_group]['startPoint'] = False
