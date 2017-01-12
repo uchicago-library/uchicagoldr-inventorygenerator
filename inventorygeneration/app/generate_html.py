39739fffafafd294cb8e5d27b40d766094eca0b4
Tyler Danstrom <tdanstrom@uchicago.edu>
2017-01-12 16:03:28 -0600

    finished reorg of inventory generation per new requirements

--- inventorygeneration/app/generate_html.py
+++ inventorygeneration/app/generate_html.py
@@ -1,28 +1,25 @@
-from collections import namedtuple
-from jinja2 import Environment, FileSystemLoader
-from json import dumps
-from os import mkdir, listdir
-from os.path import join, basename
-from sys import argv
+"""a module to generate html inventory pages of an accession in the LDR with permanent private URLs
+"""
 
+from collections import namedtuple
+from os import mkdir
+from os.path import join, basename
+from json import dumps
+from jinja2 import Environment, FileSystemLoader
+
+from pypremis.lib import PremisRecord
 from uchicagoldrtoolsuite.bit_level.lib.readers.filesystemarchivereader import FileSystemArchiveReader
 
+ID = "0e10565a460846f8b0a3ad9c415911c9"
+
 ENV = Environment(loader=FileSystemLoader("./templates"))
-
-print(ENV.list_templates())
-print(dir(ENV))
 LONGTERM_ROOT = "/data/repository/longTermStorage"
-
 ACCESSION_URL_BASE = "https://y2.lib.uchicago.edu/ldraccession/"
 URL_BASE = "https://y2.lib.uchicago.edu/processor/"
 
 def main(longterm,arkid, num_files_per_segment=None):
-    print(longterm)
-    n = 2
-    arkid_split = [arkid[i:i+n] for i in range(0, len(arkid), n)]
     reader = FileSystemArchiveReader(longterm, arkid)
     archive = reader.read()
-
     landing_page = namedtuple("landing_page",
                               "accession_id collection_title description segments accession_record")
     msuites = archive.get_materialsuite_list()
@@ -33,14 +30,16 @@ def main(longterm,arkid, num_files_per_segment=None):
         num_files_per_segment = 5
     total_msuites = len(msuites)
     count = 0
-    for n in msuites:
+    for msuite in msuites:
         count += 1
+        precord = PremisRecord(frompath=str(msuite.premis.path))
+        original_name = precord.objects_list[0].get_originalName()
         n_tuple = namedtuple("an_item",
-                             "name contenturl premisurl")(n.content.item_name,
+                             "name contenturl premisurl")(original_name,
                                                           URL_BASE + arkid + "/" +\
-                                                            n.identifier + "/content",
+                                                            msuite.identifier + "/content",
                                                           URL_BASE + arkid + "/" +\
-                                                            n.identifier + "/premis")
+                                                            msuite.identifier + "/premis")
 
         current_bunch.append(n_tuple)
         tally += 1
@@ -56,32 +55,39 @@ def main(longterm,arkid, num_files_per_segment=None):
                               len(all_bunches) + 1)
             ]
     pages_dict = {}
-    for n in pages:
-        pages_dict[str(n)] = {"active":True, "startPoint":True}
-    print(pages_dict)
+    for page in pages:
+        pages_dict[str(page)] = {"active":True, "startPoint":True}
     json_string = dumps(pages_dict)
     mkdir("./" + arkid)
     for n_bunch in all_bunches:
         tally_id = "{}.html".format(tally_bunch)
-        print(tally_id)
-        print(len(n_bunch))
         total += len(n_bunch)
         tally_bunch += 1
         pages_dict[str(tally_bunch)]['active'] = True
         pages_dict[str(tally_bunch)]['startPoint'] = True
+        pages_dict[str(tally_bunch)]['numfiles'] = len(n_bunch)
         segment_template = ENV.get_template("section_list.html")
-        segment_html = segment_template.render(arkid=arkid, label=str(tally_bunch),
+        segment_html = segment_template.render(arkid=arkid,
+                                               label=str(tally_bunch),
                                                files=n_bunch,
                                                pagerecord=json_string)
         with open(join(arkid, tally_id), "w") as a_file_to_write:
             a_file_to_write.write(segment_html)
     landing_template = ENV.get_template("accession_landing.html")
+    pages = sorted([(x, pages_dict[x].get("numfiles"))
+                    for x in pages_dict.keys() if pages_dict[x].get("numfiles")
+                   ],
+                   key=lambda x: x[0])
     landing_html = landing_template.render(arkid=arkid,
-                                           accessions=[basename(str(x.path))
+                                           pages=pages,
+                                           accessions=[ACCESSION_URL_BASE + "/accession/" +
+                                                       basename(str(x.path))
                                                        for x in archive.get_accessionrecord_list()],
-                                           legalnotes=[basename(str(x.path))
+                                           legalnotes=[ACCESSION_URL_BASE + "/legalnote/" +
+                                                       basename(str(x.path))
                                                        for x in archive.get_legalnote_list()],
-                                           adminnotes=[basename(str(x.path))
+                                           adminnotes=[ACCESSION_URL_BASE + "/adminnote/" +
+                                                       basename(str(x.path))
                                                        for x in archive.get_adminnote_list()])
     with open(join(arkid, "index.html"), "w") as write_file:
         write_file.write(landing_html)
